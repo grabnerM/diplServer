46cc0b37a900801eddd9bc177f8e82273db47d18
Jakob Hocheneder <jakob.hocheneder@gmail.com>
2020-10-28 15:56:34 +0100

    refresh token

--- PTS/middleware.ts
+++ PTS/middleware.ts
@@ -18,6 +18,7 @@ exports.ensureToken = function (req: { headers: { [x: string]: any }; token: any
         //use the jwt.verify method to verify the access token
         //throws an error if the token has expired or has a invalid signature
         payload = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
+        console.log(payload)
         next()
     }
     catch(e){
@@ -28,14 +29,14 @@ exports.ensureToken = function (req: { headers: { [x: string]: any }; token: any
 }
 
 exports.ensureRefreshToken = function (req: { headers: { [x: string]: any }; token: any }, res: { sendStatus: (arg0: number) => any; status: (arg0: number) => { (): any; new(): any; send: { (): any; new(): any } } }, next: any){
-    const authHeader = req.headers['authorization']
+    const authHeader = req.headers['refresh']
     const token = authHeader && authHeader.split(' ')[1]
     req.token = token
     if (token == null){
         console.log("token null")
         return res.sendStatus(401)
     }
-    
+
     if (!token){
         return res.status(403).send()
     }
--- build/middleware.js
+++ build/middleware.js
@@ -18,6 +18,7 @@ exports.ensureToken = function (req, res, next) {
         //use the jwt.verify method to verify the access token
         //throws an error if the token has expired or has a invalid signature
         payload = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+        console.log(payload);
         next();
     }
     catch (e) {
@@ -27,7 +28,7 @@ exports.ensureToken = function (req, res, next) {
     }
 };
 exports.ensureRefreshToken = function (req, res, next) {
-    var authHeader = req.headers['authorization'];
+    var authHeader = req.headers['refresh'];
     var token = authHeader && authHeader.split(' ')[1];
     req.token = token;
     if (token == null) {
